58970c069665df95478ae40699ddff384cfd5b09
gntcjswo <webfavorite@naver.com>
2026-05-06 22:15:29 +0900

    개인 프로젝트 설명글 변경

--- src/pages/PersonalPage/index.jsx
+++ src/pages/PersonalPage/index.jsx
@@ -9,17 +9,17 @@ import styles from './PersonalPage.module.scss';
 export function PersonalPage() {
   const clearFilters = useFilterStore((s) => s.clearFilters);
   const pageHeaderRef = usePageAnimation();
-  useEffect(() => { clearFilters(); }, [clearFilters]);
+  useEffect(() => {
+    clearFilters();
+  }, [clearFilters]);
   return (
     <main className={styles.page}>
       <div className={styles.inner}>
         <header ref={pageHeaderRef} className={styles.pageHeader}>
           <h1 className={styles.heading}>Personal</h1>
-          <p className={styles.subHeading}>
-            개인적으로 진행한 사이드 프로젝트와 실험적인 작업들
-          </p>
+          <p className={styles.subHeading}>개인적으로 진행한 사이드 프로젝트와 실험적인 작업들입니다.</p>
         </header>
-        <ProjectGrid useData={usePersonalProjects} variant="personal" />
+        <ProjectGrid useData={usePersonalProjects} variant='personal' />
       </div>
       <ProjectModal />
     </main>
