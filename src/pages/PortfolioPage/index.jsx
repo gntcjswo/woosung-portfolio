be744b83ba7f84ec9401b513f7cac30ff4901650
gntcjswo <webfavorite@naver.com>
2026-05-06 22:14:03 +0900

    포트폴리오 페이지 설명글 변경

--- src/pages/PortfolioPage/index.jsx
+++ src/pages/PortfolioPage/index.jsx
@@ -9,15 +9,15 @@ import styles from './PortfolioPage.module.scss';
 export function PortfolioPage() {
   const clearFilters = useFilterStore((s) => s.clearFilters);
   const pageHeaderRef = usePageAnimation();
-  useEffect(() => { clearFilters(); }, [clearFilters]);
+  useEffect(() => {
+    clearFilters();
+  }, [clearFilters]);
   return (
     <main className={styles.page}>
       <div className={styles.inner}>
         <header ref={pageHeaderRef} className={styles.pageHeader}>
           <h1 className={styles.heading}>Portfolio</h1>
-          <p className={styles.subHeading}>
-            2015년부터 쌓아온 경험으로 완성된 프로젝트들
-          </p>
+          <p className={styles.subHeading}>2024년 이후 진행한 프로젝트만 선별하여 수록했습니다.</p>
         </header>
         <ProjectGrid useData={useProjects} />
       </div>
